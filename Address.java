7d8fde7c24aa3a565977c5ffc3e2440df76d6425
Joshua Dupin <joshua35260@gmail.com>
2023-02-27 15:50:51 +0100

    ok

---
+++ Address.java
@@ -0,0 +1,6 @@
+public class Address {
+  public int number;
+  public String street;
+  public String city;
+  public String country;
+}
---
+++ User.java
@@ -0,0 +1,11 @@
+import java.util.List;
+import java.util.Date;
+import java.util.ArrayList;
+
+
+public class User {
+  public Date birthdate;
+  public int size;
+  public List<Photo> photos = new ArrayList<>();
+  public Address address;
+}
